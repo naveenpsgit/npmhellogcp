5708b23985b8dd7b9835ad005f2f726497b6cb3b
Naveen PS <37842303+naveenpsgit@users.noreply.github.com>
2020-04-07 14:23:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-        res.send('Hello World! h');
+        res.send('Hello World! hi');
     });
 
 const port = 3000;
